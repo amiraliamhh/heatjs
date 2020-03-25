7486aa88a2c76ab4e150d6a5a592d710d1e08926
amiraliamhh <amiraliamhh@gmail.com>
2020-03-26 01:06:42 +0430

    feat: drawCircle function

--- examples/simple/script.js
+++ examples/simple/script.js
@@ -1,2 +1,3 @@
 const canvas = document.querySelector('#canvas')
 const hm = new Heatmap(canvas, [])
+hm.drawHeatPoint(40, 50)
--- src/heatmap.ts
+++ src/heatmap.ts
@@ -4,13 +4,41 @@ interface HeatmapData {
   value: number
 }
 
+interface DrawCircleOptions {
+  color: string
+  blurSize?: string
+}
+
 class Heatmap {
   protected data: HeatmapData[]
   protected el: HTMLCanvasElement
+  protected ctx: CanvasRenderingContext2D
 
   constructor(element: HTMLCanvasElement, data: HeatmapData[]) {
     this.data = data
     this.el = element
+    this.ctx = this.el.getContext('2d') as CanvasRenderingContext2D
+  }
+
+  public drawHeatPoint(x: number, y: number) {
+    this.drawCircle(x, y, 40, { color: 'rgb(255,0,0)' })
+  }
+
+  protected drawCircle(
+    x: number,
+    y: number,
+    radius: number,
+    {
+      color,
+      blurSize = '2px'
+    }: DrawCircleOptions
+  ) {
+    this.ctx.beginPath()
+    this.ctx.fillStyle = color
+    this.ctx.filter = `blur(${blurSize})`
+    this.ctx.arc(x, y, radius, 0, 2 * Math.PI, false)
+    this.ctx.fill()
+    this.ctx.closePath()
   }
 }
 
