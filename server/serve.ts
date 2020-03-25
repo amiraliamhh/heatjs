232fda91b1dfb50f90422c69ddf3aaae9372d605
amiraliamhh <amiraliamhh@gmail.com>
2020-03-25 18:12:21 +0430

    initial

---
+++ server/serve.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import path from 'path'
+import config from 'config'
+
+const app = express()
+const examplesPath = path.resolve(__dirname, '..', 'examples')
+
+app.use(express.static(examplesPath))
+
+const PORT = config.get('port')
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
---
+++ src/heatmap.ts
@@ -0,0 +1,15 @@
+interface HeatmapData {
+  x: number
+  y: number
+  value: number
+}
+
+class Heatmap {
+  protected data: HeatmapData[]
+  protected el: HTMLCanvasElement
+
+  constructor(element: HTMLCanvasElement, data: HeatmapData[]) {
+    this.data = data
+    this.el = element
+  }
+}
