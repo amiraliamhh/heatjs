aec7e735a2828417dbcd5a990f15fde9f2db7a59
amiraliamhh <amiraliamhh@gmail.com>
2020-03-28 03:20:31 +0430

    doc: example with 300 random points

--- examples/simple/script.js
+++ examples/simple/script.js
@@ -1,12 +1,10 @@
+function generateRandomPoints(num) {
+  return new Array(num).fill({}).map(a => ({
+    x: Math.floor(Math.random() * 600),
+    y: Math.floor(Math.random() * 300)
+  }))
+}
+
 const canvas = document.querySelector('#canvas')
-const hm = new Heatmap(canvas, [
-  {
-    x: 40,
-    y: 40
-  },
-  {
-    x: 40,
-    y: 45
-  }
-])
+const hm = new Heatmap(canvas, generateRandomPoints(300))
 hm.drawHeatmap()
